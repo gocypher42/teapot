e91ad48ea1ad03a66ed8eedda077452501da626a
Olivier Guenette <olivierguenette@hotmail.com>
2023-08-29 15:22:27 -0400

    init

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <fmt/core.h>
+
+class App
+{
+public:
+  int Run(int argc, char *argv[]);
+
+private:
+};
+
+int App::Run(int argc, char *argv[])
+{
+  fmt::print("Hello {}!\n", "World");
+
+  return 0;
+};
+
+int main(int argc, char *argv[])
+{
+  App app;
+  return app.Run(argc, argv);
+}
