d1dfd2a664ac484e1b63dd11fb000a00cd11fe8c
Olivier Guenette <olivierguenette@hotmail.com>
2023-10-25 21:24:09 -0400

    add test

--- src/project_builder/project_file.h
+++ src/project_builder/project_file.h
@@ -9,6 +9,7 @@ class ProjectFile final
 public:
   explicit ProjectFile(string file_name);
 
+  void set_name(const std::string &name) noexcept;
   void set_content(const std::string &content) noexcept;
   [[nodiscard]] const string &get_name() const noexcept;
   [[nodiscard]] const string &get_content() const noexcept;
